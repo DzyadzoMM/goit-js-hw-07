606126993ce921e12f0fc70f69724d9d4845d784
DzyadzoMM <m.dziadzo@gmail.com>
2025-05-13 19:14:51 +0300

    task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", event => {
+	event.preventDefault();
+	const formEl = form.elements;
+	const emailInput = formEl.email;
+	const passwordInput = formEl.password;
+	const emailValue = emailInput.value.trim();
+	const passwordValue = passwordInput.value.trim();
+	if(emailValue === ''|| passwordValue === ''){
+		alert("All form fields must be filled in");
+		return
+	}
+	const formData = {
+		email: emailValue,
+		password: passwordValue
+	};
+	console.log(formData);
+	form.reset();
+});
 
+	
